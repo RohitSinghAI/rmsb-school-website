e1ab395434c1b5fca8c068ff807989a7dcdf0717
rohit singh <rk6109744@gmail.com>
2026-01-13 01:59:04 -0500

    update

--- client/middleware.js
+++ client/middleware.js
@@ -3,7 +3,6 @@ import { NextResponse } from "next/server";
 export function middleware(req) {
   const { pathname } = req.nextUrl;
 
-  // 🚫 Dashboard ko middleware se chhod do
   if (pathname.startsWith("/admin/dashboard")) {
     return NextResponse.next();
   }
